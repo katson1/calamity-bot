88221155bc1a58183e5e14a7cc4fe95fc3301af3
Katson Alves <katson.alves@ccc.ufcg.edu.br>
2024-04-03 18:52:23 -0300

    bo7 - improving embed file

--- utils/embed.js
+++ utils/embed.js
@@ -1,65 +1,55 @@
-export const getEmbed = () => {
-    
+const createEmbed = ({ color = 0x000000, title = '', description = '', footerText = '' } = {}) => {
     const embed = {
-        color: 0x000000,
-        title: '',
-        description: '',
+        color,
+        title,
+        description,
         fields: [],
     };
 
+    if (footerText) {
+        embed.footer = { text: footerText };
+    }
+
     return embed;
-}
+};
+
+export const getEmbed = () => { return createEmbed(); };
 
 export const getEmbedDev = () => {
-    
-    const embed = {
+    return createEmbed({
         color: 0x2F4F4F,
-        title: '',
-        description: '',
-        fields: [],
-        footer: {
-            text: `Developed by @katson (github.com/katson1)`,
-        },
-    };
-
-    return embed;
-}
+        footerText: 'Developed by @katson (github.com/katson1)'
+    });
+};
 
 export const getEmbedGamesFinished = () => {
-    
-    const embed = {
-        color: 0x000000,
+    return createEmbed({
         title: 'Games finished!',
-        description: '',
-        fields: [],
-        footer: {
-            text: ``,
-        },
-    };
+    });
+};
 
-    return embed;
-}
 
 export const createGamesEmbed = (matchSetup) => {
-    const gamesEmbed = getEmbed();
-    gamesEmbed.title = `Games:`;
-    gamesEmbed.description = `${matchSetup.mode}: \`${matchSetup.team1Controller.wins} - ${matchSetup.team2Controller.wins}\``;
+    const gamesEmbed = createEmbed({
+        title: `Games:`,
+        description: `${matchSetup.mode}: \`${matchSetup.team1Controller.wins} - ${matchSetup.team2Controller.wins}\``,
+    });
     
     matchSetup.games.forEach((game, index) => {
         gamesEmbed.fields.push(
-            { name: `\u200b`, value: ``, inline: false  },
-            { name: `Game \`${index+1}\`:`, value: ``, inline: false  }
+            { name: `\u200b`, value: ``, inline: false },
+            { name: `Game \`${index + 1}\`:`, value: ``, inline: false }
         );
-        if(game.progress == 1 || game.progress == 2){
+        if (game.progress == 1 || game.progress == 2) {
             gamesEmbed.fields.push(
-                { name: ``, value: `**Map:** \`${game.mapPlayed}\``, inline: false  },
-                { name: ``, value: `**First Pick:** \`${game.fp.name}\``, inline: false  }
+                { name: ``, value: `**Map:** \`${game.mapPlayed}\``, inline: false },
+                { name: ``, value: `**First Pick:** \`${game.fp.name}\``, inline: false }
             );
         } else {
             gamesEmbed.fields.push(
-                { name: ``, value: `**Map:** \`${game.mapPlayed}\``, inline: false  },
-                { name: ``, value: `**First Pick:** \`${game.fp.name}\``, inline: false  },
-                { name: ``, value: `**Winner:** ${game.winner}`, inline: false  },
+                { name: ``, value: `**Map:** \`${game.mapPlayed}\``, inline: false },
+                { name: ``, value: `**First Pick:** \`${game.fp.name}\``, inline: false },
+                { name: ``, value: `**Winner:** ${game.winner}`, inline: false },
             );
         }
     });
@@ -68,61 +58,64 @@ export const createGamesEmbed = (matchSetup) => {
 }
 
 export const createPicksEmbed = (currentPickUser) => {
-    const pickEmbed = getEmbed();
-    pickEmbed.title = "Map pick:";
-    pickEmbed.description = `${currentPickUser}, please select a map to play:`;
-    return pickEmbed;
+    return createEmbed({
+        title: "Map pick:",
+        description: `${currentPickUser}, please select a map to play:`,
+    });
 }
 
 export const createStaffEmbed = () => {
-    const staffEmbed = getEmbed();
-    staffEmbed.title = '';
-    staffEmbed.description = `Administrators, please designate the winner:`;
-    return staffEmbed;
+    return createEmbed({
+        description: 'Administrators, please designate the winner:',
+    });
 }
 
 export const createBansEmbed = (currentBanUser) => {
-    const bansEmbed = getEmbed();
-    bansEmbed.title = "Map bans";
-    bansEmbed.description = `${currentBanUser}, please select a map to ban:`;
-    return bansEmbed;
+    return createEmbed({
+        title: "Map bans",
+        description: `${currentBanUser}, please select a map to ban:`,
+    });
 }
 
 export const createMapBanned = (currentBanUser, banned) => {
-    const bansEmbed = getEmbed();
-    bansEmbed.description = `${currentBanUser}, has banned: **${banned}**`;
-    return bansEmbed;
+    return createEmbed({
+        description: `${currentBanUser}, has banned: **${banned}**`,
+    });
 }
-
 export const createLoserDecisionEmbed = (currentPickUser) => {
-    const pickEmbed = getEmbed();
-    pickEmbed.title = "Loser decision:";
-    pickEmbed.description = `${currentPickUser}, please select:`;
-    return pickEmbed;
+    return createEmbed({
+        title: "Loser decision:",
+        description: `${currentPickUser}, please select:`,
+    });
 }
 
 export const embedMention = (user, system) => {
-    const embed = getEmbed();
     const userMention = `<@${user.id}>`;
-    embed.description = `${userMention} have selected: **${system}**`;
-    embed.footer = null;
-    return embed;
+    return createEmbed({
+        description: `${userMention} have selected: **${system}**`,
+    });
 }
 
-export const newEmbedRegistration = (matchSetup, description) => {
-    const embed = getEmbed();
-    embed.title = `Match: **${matchSetup.team1Controller.role.name}** vs **${matchSetup.team2Controller.role.name}**`;
-    if (description == 1) {
-        embed.description = `Captains, click \`join\` to represent your team.`;
-    } else {
-        embed.description = `Teams ready!`;
-    }
 
-    const team1Status = matchSetup.team1Controller.user ? `<@&${matchSetup.team1Controller.role.id}> (captain: <@${matchSetup.team1Controller.user.id}>)` : `<@&${matchSetup.team1Controller.role.id}> (Waiting for captain)`;
-    const team2Status = matchSetup.team2Controller.user ? `<@&${matchSetup.team2Controller.role.id}> (captain: <@${matchSetup.team2Controller.user.id}>)` : `<@&${matchSetup.team2Controller.role.id}> (Waiting for captain)`;
+export const newEmbedRegistration = (matchSetup, description) => {
+    let embedDescription = description === 1
+        ? `Captains, click \`join\` to represent your team.`
+        : `Teams ready!`;
+
+    const embed = createEmbed({
+        title: `Match: **${matchSetup.team1Controller.role.name}** vs **${matchSetup.team2Controller.role.name}**`,
+        description: embedDescription,
+    });
+
+    const team1Status = matchSetup.team1Controller.user
+        ? `<@&${matchSetup.team1Controller.role.id}> (captain: <@${matchSetup.team1Controller.user.id}>)`
+        : `<@&${matchSetup.team1Controller.role.id}> (Waiting for captain)`;
+    const team2Status = matchSetup.team2Controller.user
+        ? `<@&${matchSetup.team2Controller.role.id}> (captain: <@${matchSetup.team2Controller.user.id}>)`
+        : `<@&${matchSetup.team2Controller.role.id}> (Waiting for captain)`;
 
     embed.fields.push(
-        { name: `\`${matchSetup.mode}\` - bans: \`${matchSetup.bans}\``, value: ``, inline: false },
+        { name: `\`${matchSetup.mode}\` - bans: \`${matchSetup.bans}\``, value: ` `, inline: false },
         { name: 'Team 1', value: team1Status, inline: true },
         { name: 'Team 2', value: team2Status, inline: true },
     );
@@ -131,26 +124,25 @@ export const newEmbedRegistration = (matchSetup, description) => {
         let firstPickTeam = matchSetup.getTeamFp();
         let mapPickTeam = matchSetup.getTeamMap();
         embed.fields.push(
-            { name: `\u200b`, value: ``, inline: false },
+            { name: `\u200b`, value: ` `, inline: false },
             { name: `Toss coin:`, value: `**First Pick:** \`${firstPickTeam.role.name} (${firstPickTeam.user.globalName})\``, inline: false },
-            { name: ``, value: `**Map:** \`${mapPickTeam.role.name} (${mapPickTeam.user.globalName})\``, inline: false },
-            { name: `\u200b`, value: ``, inline: false }
+            { name: ` `, value: `**Map:** \`${mapPickTeam.role.name} (${mapPickTeam.user.globalName})\``, inline: false },
+            { name: `\u200b`, value: ` `, inline: false }
         );
     }
 
     if (matchSetup.team1Controller.mapsBanned.length > 0) {
         const mapsBannedString = matchSetup.team1Controller.mapsBanned.join(', ');
         embed.fields.push(
-            { name: `${matchSetup.team1Controller.role.name} bans:`, value: `\`${mapsBannedString}\``, inline: true  }
+            { name: `${matchSetup.team1Controller.role.name} bans:`, value: `\`${mapsBannedString}\``, inline: true }
         );
     }
 
     if (matchSetup.team2Controller.mapsBanned.length > 0) {
         const mapsBannedString = matchSetup.team2Controller.mapsBanned.join(', ');
         embed.fields.push(
-            { name: `${matchSetup.team2Controller.role.name} bans:`, value: `\`${mapsBannedString}\``, inline: true  }
+            { name: `${matchSetup.team2Controller.role.name} bans:`, value: `\`${mapsBannedString}\``, inline: true }
         );
-        
     }
 
     return embed;

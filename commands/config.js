22618c2140ea155b984d8e68a011f073c1d82dc3
Katson Alves <katson.alves@ccc.ufcg.edu.br>
2024-04-04 22:04:55 -0300

    config - creating config command

--- commands/config.js
+++ commands/config.js
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
 import { getEmbed, getEmbedDev } from "../utils/embed.js";
+import { modifyMap, modifyBo7, getRoles, getBo7, getMaps } from "../config/config.js";
 
 export default {
     data: new SlashCommandBuilder()
@@ -13,6 +14,10 @@ export default {
                     { name: 'Maps', value: 'maps' },
                     { name: 'Bo7', value: 'bo7' },
                 )
+        )
+        .addStringOption(option =>
+            option.setName('value')
+                .setDescription('Select the value setting')
         ),
 
     async execute(interaction) {
@@ -21,13 +26,66 @@ export default {
         const configEmbed = getEmbed();
         configEmbed.color = 0x2F4F4F;
         configEmbed.title = 'Config';
-        configEmbed.fields.push(
-            {
-                name: ``,
-                value: ``,
-                inline: false,  
-            }
-        );
+
+        const setting = interaction.options.getString('setting');
+        const value = interaction.options.getString('value');
+
+        const member = interaction.guild.members.cache.get(interaction.user.id);
+        const allowedRoles = await getRoles();
+
+        if (!member.roles.cache.some(role => allowedRoles.includes(role.name.toLowerCase()))) {
+            await interaction.reply({
+                content: "Only admin roles can use /config!",
+                ephemeral: true
+            });
+            return;
+        }
+
+        if (setting == 'maps' && value) {
+            const mapList = value.split(' ')
+            const mapQuotes = value.replace(/\s+/g, ', ')
+            modifyMap(mapList);
+            configEmbed.fields.push(
+                {
+                    name: `Maps updated!`,
+                    value: mapQuotes,
+                    inline: false,
+                }
+            );
+        }
+        
+        if (setting == 'bo7' && value) {
+            const newValueBo7 = value.trim().toLowerCase()
+            modifyBo7(newValueBo7);
+            configEmbed.fields.push(
+                {
+                    name: `Bo7 setting updated!`,
+                    value: newValueBo7,
+                    inline: false,
+                }
+            );
+        }
+
+        if (setting == 'maps' && !value) {
+            const maps = await getMaps();
+            configEmbed.fields.push(
+                {
+                    name: `Current maps:`,
+                    value: maps.join(", "),
+                    inline: false,
+                }
+            );
+        }
+        
+        if (setting == 'bo7' && !value) {
+            configEmbed.fields.push(
+                {
+                    name: `Current Bo7 setting:`,
+                    value: await getBo7(),
+                    inline: false,
+                }
+            );
+        }
         await interaction.reply({ embeds: [configEmbed, devEmbed] });
     }
 };
--- commands/help.js
+++ commands/help.js
@@ -14,12 +14,26 @@ export default {
         helpEmbed.title = 'Commands:';
         helpEmbed.fields.push(
             {
-                name: `**\`/newmatch\`**`,
-                value: `Select a game mode and assign two teams (Discord roles). If the selected game mode is \`(bo7)\`, designate the \`Upper Finals Winner\` as \`team_1\`.`,
+                name: `**/newmatch**`,
+                value: `Select a game mode and assign two teams (Discord roles). If the selected game mode is \`bo7\`, designate the \`Upper Finals Winner\` as \`team_1\`.`,
                 inline: false,
             },
             {
-                name: `**\`/help\`**`,
+                name: `**/config**`,
+                value: `This command allows you to configure or retrieve settings for the bot.
+
+                To set map preferences, use the setting command followed by the desired maps enclosed in spaces. For example: Sky Alterac Cursed. Each map should be listed separately. Note that maps must be specified by a single name;
+                
+                Additionally, you can modify the format of the Bo7 system for tournaments. If your tournament utilizes the Upper Finals configuration, you can adjust the advantage given to the winning team.
+                
+                Using the setting Bo7 command, you can specify:
+                
+                **map:** Automatically sets the toss coin winner to team_1.
+                **game:** Sets the first game win to team_1.`,
+                inline: false,
+            },
+            {
+                name: `**/help**`,
                 value: `Display the command descriptions.`,
                 inline: false,
             },
